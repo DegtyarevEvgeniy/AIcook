1c0e50bc72e288f2a749cc3b280c94e8a9b4b189
lemuzme <101337495+lemuzme@users.noreply.github.com>
2022-10-12 10:26:23 +0300

    add models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class BaseProduct(models.Model):
+    type = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    basic_taste =  models.CharField(max_length=100)
+    fats = models.IntegerField()
+    carbohydrates = models.IntegerField()
+    protein = models.IntegerField()
+    glycemic_index = models.IntegerField()
+    calories = models.IntegerField()
