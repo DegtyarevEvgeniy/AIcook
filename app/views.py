0f7fb6da950169d1484906894a4333e18527c2af
lemuzme <lemuzme1@gmail.com>
2022-10-15 16:39:57 +0300

    main page

--- app/views.py
+++ app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request, 'index.html')
+
+def main_page(request):
+    return render(request, 'main.html')   
--- cook/urls.py
+++ cook/urls.py
@@ -21,5 +21,6 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index_page),
+    path('main/', views.main_page)
     
 ]
